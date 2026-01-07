69212ce8d8015dd4237331dce2f8e07cf14c8e3a
kamalia <norkamaliajs@gmail.com>
2026-01-07 16:18:42 +0800

    application.jsp - servlet & dao

---
+++ src/controller/ApplicationServlet.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/ApplyProgramServlet")
+public class ApplicationServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        //get form data
+        String name = request.getParameter("name");
+        String phoneNum = request.getParameter("phoneNum");
+        String ICnum = request.getParameter("ICnum");
+        int eventID = Integer.parseInt(request.getParameter("eventID"));
+
+        //get userID from session (if logged in)
+        HttpSession session = request.getSession(false);
+        Integer userID = null;
+
+        if (session != null && session.getAttribute("userID") != null) {
+            userID = (Integer) session.getAttribute("userID");
+        }
+
+        //DAO
+        ApplicationDao dao = new ApplicationDao();
+        boolean success = dao.insertRegistration(
+                name, phoneNum, ICnum, userID, eventID
+        );
+
+        if (success) {
+            response.sendRedirect("application.jsp?status=success");
+        } else {
+            response.sendRedirect("application.jsp?status=error");
+        }
+    }
+}
---
+++ src/dao/ApplicationDao.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class ApplicationDao {
+
+    public boolean insertRegistration(String name, String phoneNum,
+                                      String ICnum, Integer userID, int eventID) {
+
+        boolean result = false;
+
+        String sql = "INSERT INTO registration " +
+                     "(name, phoneNum, ICnum, userID, eventID) " +
+                     "VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, name);
+            ps.setString(2, phoneNum);
+            ps.setString(3, ICnum);
+
+            if (userID != null) {
+                ps.setInt(4, userID);
+            } else {
+                ps.setNull(4, java.sql.Types.INTEGER);
+            }
+
+            ps.setInt(5, eventID);
+
+            result = ps.executeUpdate() > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
